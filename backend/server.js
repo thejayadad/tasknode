441d41a602212e620059dc9c5af11d744ba6b503
thejayadad <thejayadad@gmail.com>
2023-09-11 16:20:42 -0400

    update

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,24 @@
-// server.js
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors'); // Import the cors middleware
+const cors = require('cors');
 const app = express();
 const port = 3001;
 
 app.use(bodyParser.json());
 app.use(cors());
 
-// In-memory storage for tasks (replace this with a database in production)
 const tasks = [];
 
-// API endpoint to get all tasks
 app.get('/api/tasks', (req, res) => {
     res.json(tasks);
 });
 
-// API endpoint to create a new task
 app.post('/api/tasks', (req, res) => {
     const task = req.body;
     tasks.push(task);
     res.json({ message: 'Task created successfully' });
 });
-
-// API endpoint to delete a task by its index
 app.delete('/api/tasks/:index', (req, res) => {
     const index = parseInt(req.params.index);
     if (index >= 0 && index < tasks.length) {
@@ -35,7 +29,6 @@ app.delete('/api/tasks/:index', (req, res) => {
     }
 });
 
-// API endpoint to mark a task as completed
 app.put('/api/tasks/:index/complete', (req, res) => {
     const index = parseInt(req.params.index);
     if (index >= 0 && index < tasks.length) {
@@ -46,7 +39,6 @@ app.put('/api/tasks/:index/complete', (req, res) => {
     }
 });
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
